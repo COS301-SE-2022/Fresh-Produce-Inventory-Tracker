b99735ae9977dc66239a9b4705d699d07c127adb
Raymond Boonzaier <u19014725@tuks.co.za>
2022-05-12 19:27:08 +0200

    🗃️ (repository) Added scale code

---
+++ fresh-produce-inventory-tracker/libs/api/scale/repository/src/lib/scale.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable, Param } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../../../../prisma/shared/src/lib/prismaService.service';
+
+@Injectable()
+export class AuthenicationRepository {
+  constructor(private prisma: PrismaService) {}
+  async getScale(id: number,userid: number) {
+    return await this.prisma.scale.findUnique({ where: {id_userId:{userId:userid,id:id} } });
+  }
+  async createScale(userId:number,weightfull:number,weightone:number,producetype:Prisma.EnumProduceTypeFilter) {
+    return await this.prisma.scale.create({
+      data: { userId:userId,WeightTotal:weightfull,WeightIndividual:weightone,ProduceType:producetype.equals }
+    });
+  }
+  async editScale(id: number, userid:number, data:any) {
+    return await this.prisma.scale.update({ where: { id_userId:{id: id ,userId:userid}}, data: data });
+  }
+  async removeScale(id: number, userid:number) {
+    return await this.prisma.scale.delete({ where: { id_userId:{id: id ,userId:userid}} });
+  }
+}
