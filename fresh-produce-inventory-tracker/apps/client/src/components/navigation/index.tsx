fb32ce05aa32189acb38a550a270e51e4bd1475b
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-05-16 12:53:55 +0200

    refactor

--- fresh-produce-inventory-tracker/apps/client/src/components/navigation/index.tsx
+++ fresh-produce-inventory-tracker/apps/client/src/components/navigation/index.tsx
@@ -2,6 +2,8 @@ import { MdOutlineNotifications } from 'react-icons/md';
 import Link from 'next/link';
 import { RiUser3Line } from 'react-icons/ri';
 import { useRouter } from 'next/router';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 /* eslint-disable-next-line */
 export interface NavigationProps {}
 
@@ -10,17 +12,31 @@ export function Navigation(props: NavigationProps) {
 
   return (
     <div className="flex items-center transition-all justify-between w-full h-20 shadow-md rounded-lg p-4 lg:max-w-[98%] ml-2">
-      <h1 className="text-xl font-black">
-        {router.route === '/'
-          ? 'Dashboard'
-          : `${router.pathname
-              .replace('/', '')
-              .charAt(0)
-              .toUpperCase()}${router.pathname.replace('/', '').substring(1)}`}
-      </h1>
-      <div className="flex gap-x-4">
+      <div>
+        <h1 className="text-xl font-black">
+          {router.route === '/'
+            ? 'Dashboard'
+            : `${router.pathname
+                .replace('/', '')
+                .charAt(0)
+                .toUpperCase()}${router.pathname
+                .replace('/', '')
+                .substring(1)}`}
+        </h1>
+        <p className="mt-2 text-sm text-primary/70">
+          {new Date().toString().split(' ').splice(1, 3).join(' ')}
+        </p>
+      </div>
+      <div className="flex items-center gap-x-4">
+        <label
+          htmlFor="my-drawer-2"
+          className="p-2 transition-all rounded-md cursor-pointer hover:bg-primary/20 bg-primary/10"
+        >
+          <GiHamburgerMenu className="w-full h-full cursor-pointer pointer-events-none"></GiHamburgerMenu>
+        </label>
+
         <a href="notifications">
-          <MdOutlineNotifications className="w-10 h-10 p-2 rounded-full cursor-pointer bg-neutral-10"/>
+          <MdOutlineNotifications className="w-10 h-10 p-2 rounded-full cursor-pointer bg-neutral-10" />
         </a>
         <div className="flex items-center cursor-pointer gap-x-2">
           <a href="/user">
