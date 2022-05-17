066895b362fa2786746391570dbd8475dfface78
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-05-17 22:25:32 +0200

    Added signup page

---
+++ fresh-produce-inventory-tracker/apps/client/pages/signup/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface SignupProps {}
+
+export function Signup(props: SignupProps) {
+  return (
+    <div>
+      <h1>Welcome to Signup!</h1>
+    </div>
+  );
+}
+
+export default Signup;
