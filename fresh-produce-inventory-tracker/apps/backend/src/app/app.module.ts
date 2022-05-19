6c76fd469940c3a2f6de6e23ced19d4c45090e8e
Raymond Boonzaier <u19014725@tuks.co.za>
2022-05-19 11:02:04 +0200

    (notifications) add the method to send emails

--- fresh-produce-inventory-tracker/apps/backend/src/app/app.module.ts
+++ fresh-produce-inventory-tracker/apps/backend/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { ApiAuthenticationApiModule } from '../../../../libs/api/authentication/
 import { ApiImageApiModule } from '../../../../libs/api/image/api/src/lib/api-image-api.module';
 import { ApiScaleApiModule } from '../../../../libs/api/scale/api/src/lib/api-scale-api.module'
 import { PrismaService } from 'libs/api/prisma/shared/src/lib/prismaService.service';
+import {ApiNotificationsApiModule} from '../../../../libs/api/notifications/api/src/lib/api-notifications-api.module';
 
 @Module({
-  imports: [ApiAuthenticationApiModule, ApiImageApiModule, ApiScaleApiModule],
+  imports: [ApiAuthenticationApiModule, ApiImageApiModule, ApiScaleApiModule,ApiNotificationsApiModule ],
   controllers: [AppController],
   providers: [AppService,PrismaService],
 })
