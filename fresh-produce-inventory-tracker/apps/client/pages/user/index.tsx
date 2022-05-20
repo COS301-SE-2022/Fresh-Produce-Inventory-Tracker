9bfb6bb96cb166c93d4d39b1688cbdb0092fe4bb
Durandt Uys <u18286250@up.ac.za>
2022-05-20 11:10:02 +0200

    User info page

--- fresh-produce-inventory-tracker/apps/client/pages/user/index.tsx
+++ fresh-produce-inventory-tracker/apps/client/pages/user/index.tsx
@@ -9,7 +9,7 @@ export function User(props: UserProps)
     <div className="ml-2 h-5/6 border-solid border-2 border-red-200 lg:max-w-[98%] rounded">
       <div className="grid grid-cols-4 ml-2">
         <div className="avatar my-2 ml-2">
-          <div className="w-80 rounded-l-lg border-solid border-2 border-blue-200">
+          <div className="w-80 rounded-lg border-solid border-2">
             <Image />
           </div>
         </div>
@@ -22,8 +22,22 @@ export function User(props: UserProps)
               <p className="m-2">Durandt Uys</p>
             </div>
           </div>
-          <h2 className="m-2 font-bold">Email:</h2>
-          <h2 className="m-2 font-bold">Bio:</h2>
+          <div className="flex items-center font-bold grid grid-cols-12">
+            <div className="m-2">
+              <label className="mt-4">Email:</label>
+            </div>
+            <div className="flex items-center w-96% col-span-11 bg-blue-200 rounded m-2">
+              <p className="m-2">durandtu@gmail.com</p>
+            </div>
+          </div>
+          <div className="flex items-center font-bold grid grid-cols-12 row-span-2">
+            <div className="m-2">
+              <label className="mt-4">Bio:</label>
+            </div>
+            <div className="flex w-96% col-span-11 bg-blue-200 rounded m-2 h-full">
+              <p className="m-2">----Some text----</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="border-solid border-2 border-blue-200 ml-4 rounded-lg h-inherit mr-4">
