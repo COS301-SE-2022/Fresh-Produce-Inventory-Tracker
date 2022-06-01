64106a4095ce3d527d5050243b1df01f3221ca35
Durandt Uys <u18286250@tuks.co.za>
2022-06-01 21:55:13 +0200

    Updated charts component

--- fresh-produce-inventory-tracker/apps/client/pages/trends/index.tsx
+++ fresh-produce-inventory-tracker/apps/client/pages/trends/index.tsx
@@ -5,8 +5,15 @@ import {Chart} from "./../../src/components/chart/chart"
 
 export function Trends(props: TrendsProps) {
   return (
-      <div className="chart">
-        <Chart type="Bar"></Chart>
+      <div className="grid grid-cols-2">
+        <div>
+          <h1>Fruit</h1>
+          <Chart type="Bar"></Chart>
+        </div>
+        <div>
+          <h1>Apples</h1>
+          <Chart type="Line"></Chart>
+        </div>
       </div>
   );
 }
--- fresh-produce-inventory-tracker/apps/client/src/components/chart/chart.tsx
+++ fresh-produce-inventory-tracker/apps/client/src/components/chart/chart.tsx
@@ -1,4 +1,4 @@
-import { Bar } from "react-chartjs-2";
+import { Bar ,Line, Pie } from "react-chartjs-2";
 
 import {
   Chart as ChartJS,
@@ -10,6 +10,7 @@ import {
   Title,
   Tooltip,
   Legend,
+  ArcElement,
 } from 'chart.js'
 
 ChartJS.register(
@@ -18,6 +19,7 @@ ChartJS.register(
   PointElement,
   LineElement,
   BarElement,
+  ArcElement,
   Title,
   Tooltip,
   Legend
@@ -42,7 +44,7 @@ export function Chart(props: ChartProps) {
         {
           label:"Week1",
           borderRadius:5,
-          data:[1,5,7,2],
+          data:[25,15,19,5],
           backgroundColor: [
             "rgba(255,50,50,0.5)",
             "rgba(50,255,50,0.5)",
@@ -55,12 +57,12 @@ export function Chart(props: ChartProps) {
             'rgb(255, 137, 0,1)',
             'rgb(102, 0, 255,1)'
           ],
-          barThickness:50,
+          barThickness:35,
           borderWidth: 1
         },{
           label:"Week2",
           borderRadius:5,
-          data:[3,2,9,0],
+          data:[30,10,21,7],
           backgroundColor: [
             "rgba(255,50,50,0.5)",
             "rgba(50,255,50,0.5)",
@@ -73,12 +75,12 @@ export function Chart(props: ChartProps) {
             'rgb(255, 137, 0,1)',
             'rgb(102, 0, 255,1)'
           ],
-          barThickness:50,
+          barThickness:35,
           borderWidth: 1
         },{
           label:"Week3",
           borderRadius:5,
-          data:[7,2,4,5],
+          data:[15,11,29,2],
           backgroundColor: [
             "rgba(255,50,50,0.5)",
             "rgba(50,255,50,0.5)",
@@ -91,12 +93,12 @@ export function Chart(props: ChartProps) {
             'rgb(255, 137, 0,1)',
             'rgb(102, 0, 255,1)'
           ],
-          barThickness:50,
+          barThickness:35,
           borderWidth: 1
         },{
           label:"Week4",
           borderRadius:5,
-          data:[1,8,3,3],
+          data:[30,10,25,5],
           backgroundColor: [
             "rgba(255,50,50,0.5)",
             "rgba(50,255,50,0.5)",
@@ -109,7 +111,7 @@ export function Chart(props: ChartProps) {
             'rgb(255, 137, 0,1)',
             'rgb(102, 0, 255,1)'
           ],
-          barThickness:50,
+          barThickness:35,
           borderWidth: 1
         }
       ]
@@ -121,14 +123,82 @@ export function Chart(props: ChartProps) {
       </div>
     );
   }
-  else
+  else if(props.type == "Line")
   {
+    const data = {
+      labels: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      datasets:[
+        {
+          label: "Week 1",
+          data: [7,5,6,3,4],
+          borderColor: "rgba(255,0,0,1)"
+        },
+        {
+          label: "Week 2",
+          data: [3,8,5,9,5],
+          fill: false,
+          borderColor: "rgba(0,255,0,1)"
+        },
+        {
+          label: "Week 3",
+          data: [5,2,4,2,1],
+          borderColor: "rgba(0,0,255,1)"
+        },
+        {
+          label: "Week 4",
+          data: [5,7,8,2,8],
+          fill: false,
+          borderColor: "rgbs(255,255,0,1)"
+        }
+      ]
+    }
+
     return (
       <div>
-        <h1>Welcome to Chart!</h1>
-        {props.type};
+        <Line data={data}></Line>
       </div>
     );
+  }
+  else
+  {
+    const data = {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    return(
+      <div>
+        <Pie data={data}></Pie>
+      </div>
+    )
   }
 }
 
