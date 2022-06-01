880c3046bf5dfecb03028a438e5cb3d04befb91f
Durandt Uys <u18286250@tuks.co.za>
2022-06-01 20:46:26 +0200

    Fixed fs error

--- fresh-produce-inventory-tracker/apps/client/next.config.js
+++ fresh-produce-inventory-tracker/apps/client/next.config.js
@@ -14,15 +14,3 @@ const nextConfig = {
 
 module.exports = withNx(nextConfig);
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
-}
