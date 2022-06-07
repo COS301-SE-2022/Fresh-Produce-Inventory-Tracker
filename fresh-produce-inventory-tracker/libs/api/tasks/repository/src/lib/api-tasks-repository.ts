84e757b65a2e04d2403493b725b93c750aea668a
Raymond Boonzaier <u19014725@tuks.co.za>
2022-06-07 11:19:31 +0200

    Update api-tasks-repository.ts

--- fresh-produce-inventory-tracker/libs/api/tasks/repository/src/lib/api-tasks-repository.ts
+++ fresh-produce-inventory-tracker/libs/api/tasks/repository/src/lib/api-tasks-repository.ts
@@ -4,22 +4,30 @@ import { PrismaService } from '../../../../prisma/shared/src/lib/prismaService.s
 @Injectable()
 export class tasksRepository {
   constructor(private prisma: PrismaService) {}
-  async getTasks(id:number) {
-    return await this.prisma.notification.findMany({ where: { userId:id, Type:'Task' } });
-  }
-  async getTasksMessage(id:number,message:string) {
-    return await this.prisma.notification.findFirst({ where: { userId:id, Type:'Task' ,message:message} });
-  }
-  async createTask(id:number,message:string) {
-    if(!(await this.prisma.notification.findFirst({where:{userId:id,message:message}})))
-    {
-    return await this.prisma.notification.create({
-      data: {userId:id, Type: 'Task', message :message }
+  async getTasks(id: number) {
+    return await this.prisma.notification.findMany({
+      where: { userId: id, Type: 'Task' },
     });
   }
-  else return null;
+  async getTasksMessage(id: number, message: string) {
+    return await this.prisma.notification.findFirst({
+      where: { userId: id, Type: 'Task', message: message },
+    });
   }
-  async deleteTask(id: number, message:string) {
-    return await this.prisma.notification.deleteMany({ where: { userId:id, Type:'Task',message:message }});
+  async createTask(id: number, message: string) {
+    if (
+      !(await this.prisma.notification.findFirst({
+        where: { userId: id, message: message },
+      }))
+    ) {
+      return await this.prisma.notification.create({
+        data: { userId: id, Type: 'Task', message: message },
+      });
+    } else return null;
+  }
+  async deleteTask(id: number, message: string) {
+    return await this.prisma.notification.deleteMany({
+      where: { userId: id, Type: 'Task', message: message },
+    });
   }
 }
