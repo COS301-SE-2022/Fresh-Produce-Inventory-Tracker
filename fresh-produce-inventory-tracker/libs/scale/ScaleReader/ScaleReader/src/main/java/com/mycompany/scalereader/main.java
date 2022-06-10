9bd62bf1f53d1948c4c0100244f0ad3db5ff9846
PapHein69 <u20436077@tuks.co.za>
2022-06-10 11:15:08 +0200

    Serialportlistener()
    Get data from COM* and if change is detected update database

--- fresh-produce-inventory-tracker/libs/scale/ScaleReader/ScaleReader/src/main/java/com/mycompany/scalereader/main.java
+++ fresh-produce-inventory-tracker/libs/scale/ScaleReader/ScaleReader/src/main/java/com/mycompany/scalereader/main.java
@@ -37,6 +37,45 @@ public class main {
         }
     }
     
-   
+    private static class PortReader implements SerialPortEventListener {
+
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            if(event.isRXCHAR() && event.getEventValue() > 0) {
+                try {
+                    String receivedData = serialPort.readString(event.getEventValue());
+                    System.out.println(receivedData);
+                    if(!receivedData.contains("-") && receivedData.length() < 3 && Integer.parseInt(receivedData) != weight && receivedData.length() == (weight+"").length())
+                    {
+                        if(Integer.parseInt(receivedData) == (weight-1) || Integer.parseInt(receivedData) == (weight-2) || Integer.parseInt(receivedData) == (weight+1) || Integer.parseInt(receivedData) == (weight+2))
+                        {
+                            weight = Integer.parseInt(receivedData);
+                            
+                            HashMap<String, String> parameters = new HashMap<>();
+
+                            parameters.put("id", "1");
+                            parameters.put("userid", "1");
+                            parameters.put("weight", weight+"");
+
+                            String form = parameters.entrySet()
+                                .stream()
+                                .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
+                                    .collect(Collectors.joining("&"));
+
+                            HttpClient client = HttpClient.newHttpClient();
+                            HttpRequest request = HttpRequest.newBuilder()
+                                    .uri(URI.create("http://localhost:3333/api/scale/editscale?id=1&userid=1&weight=" + weight))
+                                    .header("Content-Type", "application/x-www-form-urlencoded")
+                                    .POST(HttpRequest.BodyPublishers.ofString(form))
+                                    .build();
+
+                            HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+                            System.out.println(response.body());
+                        }
+                    }
+                }
+        }
+    }
   
 }
