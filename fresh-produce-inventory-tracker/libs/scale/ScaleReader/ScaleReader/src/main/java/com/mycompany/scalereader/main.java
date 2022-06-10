c55305df0a54ef9421983e6a0f456440c74527eb
PapHein69 <u20436077@tuks.co.za>
2022-06-10 11:13:17 +0200

    main.java update
    Attempt to read from port connected to ardiuno with scale

--- fresh-produce-inventory-tracker/libs/scale/ScaleReader/ScaleReader/src/main/java/com/mycompany/scalereader/main.java
+++ fresh-produce-inventory-tracker/libs/scale/ScaleReader/ScaleReader/src/main/java/com/mycompany/scalereader/main.java
@@ -13,5 +13,30 @@ import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class main {
+      static SerialPort serialPort = new SerialPort("COM8");
+    static int weight = 10;
+    
+    public static void main(String[] args) {
+        try {
+            serialPort.openPort();
+
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                                 SerialPort.DATABITS_8,
+                                 SerialPort.STOPBITS_1,
+                                 SerialPort.PARITY_NONE);
+
+            serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | 
+                                          SerialPort.FLOWCONTROL_RTSCTS_OUT);
+
+            serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+            
+            
+        }
+        catch (SerialPortException ex) {
+            System.out.println("There are an error on writing string to port т: " + ex);
+        }
+    }
+    
+   
   
 }
