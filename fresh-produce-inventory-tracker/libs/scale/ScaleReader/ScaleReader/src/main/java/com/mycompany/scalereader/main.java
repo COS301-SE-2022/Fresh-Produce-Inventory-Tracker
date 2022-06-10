8b7fd9a3362b463eab29311fae2cd7c40b9c5e23
PapHein69 <u20436077@tuks.co.za>
2022-06-10 11:11:31 +0200

    Scale reading file
    This will be used to send scale data to the system

---
+++ fresh-produce-inventory-tracker/libs/scale/ScaleReader/ScaleReader/src/main/java/com/mycompany/scalereader/main.java
@@ -0,0 +1,17 @@
+package com.mycompany.scalereader;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jssc.*;
+import java.net.*;
+import java.io.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class main {
+  
+}
