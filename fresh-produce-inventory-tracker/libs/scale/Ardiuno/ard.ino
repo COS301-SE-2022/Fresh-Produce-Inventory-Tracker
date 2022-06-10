12d8cdf3aace8d5899488e9c896394c4507be24c
PapHein69 <u20436077@tuks.co.za>
2022-06-10 10:30:16 +0200

    Update ard.ino
    add loop() to get data stream

--- fresh-produce-inventory-tracker/libs/scale/Ardiuno/ard.ino
+++ fresh-produce-inventory-tracker/libs/scale/Ardiuno/ard.ino
@@ -34,3 +34,37 @@ void setup() {
   while (!LoadCell.update());
   calibrate(); //start calibration procedure
 }
+
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      float i = LoadCell.getData();
+      //Serial.print("Load_cell output val: ");
+      Serial.print(round(i));delay(100);
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  // receive command from serial terminal
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay(); //tare
+    else if (inByte == 'r') calibrate(); //calibrate
+    else if (inByte == 'c') changeSavedCalFactor(); //edit calibration value manually
+  }
+
+  // check if last tare operation is complete
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+
+}
