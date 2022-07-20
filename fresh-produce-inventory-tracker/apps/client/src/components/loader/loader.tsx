ff3200b4d5c1a556aca8bf35b03526e02b2a2aad
Durandt Uys(U18286250) <81296952+DurandtUys@users.noreply.github.com>
2022-07-20 10:25:33 +0200

    Updated loader

--- fresh-produce-inventory-tracker/apps/client/src/components/loader/loader.tsx
+++ fresh-produce-inventory-tracker/apps/client/src/components/loader/loader.tsx
@@ -1,13 +1,62 @@
-import styles from './loader.module.css';
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment} from 'react';
 /* eslint-disable-next-line */
-export interface LoaderProps {}
+export interface LoaderProps {
+  isOpen?: boolean;
+  closeLoader?: any;
+  openLoader?: any;
+  title?: string;
+  description?: string;
+}
 
 export function Loader(props: LoaderProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Loader!</h1>
-    </div>
+      <Transition appear show={props.isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={props.closeLoader}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-full p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-lg p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    {props.title}
+                  </Dialog.Title>
+
+                  <h2>Hello</h2>
+
+                  <div className="mt-6">
+                    <p className="text-sm text-gray-500">{props.description}</p>
+                  </div>
+
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
   );
 }
 
