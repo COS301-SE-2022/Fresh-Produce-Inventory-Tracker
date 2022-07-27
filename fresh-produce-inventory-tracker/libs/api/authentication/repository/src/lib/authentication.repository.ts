7efab34adbdc7f4887dcc8ffaa8bebb08b1ca47d
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-27 15:00:40 +0200

    refactor Signup to singin user after successfull signup

--- fresh-produce-inventory-tracker/libs/api/authentication/repository/src/lib/authentication.repository.ts
+++ fresh-produce-inventory-tracker/libs/api/authentication/repository/src/lib/authentication.repository.ts
@@ -8,11 +8,11 @@ export class AuthenicationRepository {
     return await this.prisma.user.findUnique({ where: { email: email } });
   }
   async createUser(email: string, password: string, salt: string, name: string, surname: string) {
-    return await this.prisma.user.create({
-      data: { email: email, password: password, passwordSalt: salt }
+    return this.prisma.user.create({
+      data: { email: email, password: password, passwordSalt: salt, Name: name, Surname: surname }
     });
   }
   async editUser(id: number, data) {
-    return await this.prisma.user.update({ where: { id: id }, data: data });
+    return this.prisma.user.update({ where: { id: id }, data: data });
   }
 }
--- fresh-produce-inventory-tracker/libs/api/authentication/service/src/lib/authentication.service.ts
+++ fresh-produce-inventory-tracker/libs/api/authentication/service/src/lib/authentication.service.ts
@@ -1,4 +1,5 @@
-import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { ConflictException, ForbiddenException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { authenticationDto } from './authentication.dto';
 import { AuthenicationRepository } from '../../../repository/src/lib/authentication.repository';
 import { JwtService } from '@nestjs/jwt';
@@ -10,25 +11,39 @@ import bcrypt = require('bcrypt');
 export class AuthenticationService {
 
   constructor(private repo: AuthenicationRepository, private jwt: JwtService, private config: ConfigService) { }
-  async signup(email: string, password: string) {
-    //encrypt
-    const salt = '10';
-    const hash = await bcrypt.hash(password,10)
-    return await this.repo.createUser(email, hash, salt);
+  async signup(email: string, password: string, name: string, surname: string) {
+    try {
+      const salt = '10';
+      const hash = await bcrypt.hash(password, 10)
+      const data = await this.repo.createUser(email, hash, salt, name, surname);
+      if (data) {
+        const dto = new authenticationDto()
+        dto.email = email;
+        dto.password = password;
+        return this.signin(dto);
+      }
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") throw new ConflictException();
+      else throw new InternalServerErrorException();
+    }
   }
+
   async signin(dto: authenticationDto) {
-    const user = await this.repo.getUser(dto.email);
-    if (!user) throw new UnauthorizedException('Password incorrect');
+    const user = await this.repo.getUser(dto.email.toLowerCase());
+    if (!user) throw new UnauthorizedException('Email/Password incorrect.');
     const password = user.password;
-    if ((await bcrypt.compare(dto.password, password))== false) throw new UnauthorizedException('Password incorrect');
-    return this.setToken(user.id, user.email);
+    if ((await bcrypt.compare(dto.password, password)) == false) throw new UnauthorizedException('Email/Password incorrect.');
+    return {
+      token: await this.setToken(user.id, user.email, user.Name, user.Surname)
+    };
   }
-  async setToken(id: number, email: string) {
-    const data = { sub: id, email };
+
+  async setToken(id: number, email: string, name: string, surname: string) {
+    const data = { id: id, email, name: name, surname: surname };
     const secret = await this.config.get('JWT_SECRET');
-    const token = await this.jwt.signAsync(data, { expiresIn: '15m', secret: secret });
-    return { id: id, Access: token };
+    return this.jwt.signAsync(data, { expiresIn: '15m', secret: Buffer.from(secret).toString("base64") });
   }
+
   async getUser(email: string) {
     const user = await this.repo.getUser(email);
     if (!user) throw new ForbiddenException('Email not found!');
@@ -36,6 +51,7 @@ export class AuthenticationService {
     delete user.passwordSalt;
     return user;
   }
+  
   async editPassword(email: string, password: string) {
     const user = await this.repo.getUser(email);
     if (!user) throw new ForbiddenException('Email not found!');
