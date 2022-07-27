ecd0639a757d42a0f6ceca50626709df1663fe3a
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-27 19:23:09 +0200

    Add JWTModule

--- fresh-produce-inventory-tracker/libs/api/authentication/repository/src/lib/-api-authentication--repository.module.ts
+++ fresh-produce-inventory-tracker/libs/api/authentication/repository/src/lib/-api-authentication--repository.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'libs/api/prisma/shared/src/lib/prismaService.service';
 
 @Module({
-  imports:[PrismaService]
+  imports: [PrismaService],
   controllers: [],
   providers: [],
   exports: [],
 })
-export class ApiAuthenticationRepositoryModule {}
+export class ApiAuthenticationRepositoryModule { }
--- fresh-produce-inventory-tracker/libs/api/profile/api/src/lib/api-profile-api.module.ts
+++ fresh-produce-inventory-tracker/libs/api/profile/api/src/lib/api-profile-api.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { profileController } from './profile.controller';
 import { profileService } from '../../../service/src/lib/profile.service';
 import { PrismaService } from '../../../../prisma/shared/src/lib/prismaService.service';
-import {profileRepository} from '../../../repository/src/lib/profile.repository';
+import { profileRepository } from '../../../repository/src/lib/profile.repository';
+import { JwtModule } from '@nestjs/jwt';
+
+
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [profileController],
-  providers: [profileService,PrismaService,profileRepository],
+  providers: [profileService, PrismaService, profileRepository],
   exports: [],
 })
-export class ApiProfileApiModule {}
+export class ApiProfileApiModule { }
