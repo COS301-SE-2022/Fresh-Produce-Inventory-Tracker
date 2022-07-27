f03390d4bc31ee4877d314bb1056c5d1479b0fa2
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-27 19:24:13 +0200

    return only required profile data

--- fresh-produce-inventory-tracker/libs/api/profile/repository/src/lib/profile.repository.ts
+++ fresh-produce-inventory-tracker/libs/api/profile/repository/src/lib/profile.repository.ts
@@ -3,57 +3,64 @@ import { PrismaService } from '../../../../prisma/shared/src/lib/prismaService.s
 
 @Injectable()
 export class profileRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
   async getprofile(id: number) {
-    return await this.prisma.user.findFirst({
+    return this.prisma.user.findFirst({
       where: { id: +id },
+      select: {
+        Bio: true,
+        Name: true,
+        Surname: true,
+        createdAt: true,
+        email: true,
+        updatedAt: true,
+      }
     });
   }
   async editName(id: number, name: string) {
     return await this.prisma.user.update({
-      where: { id: +id },data:{Name: name}
+      where: { id: +id }, data: { Name: name }
     });
   }
   async editSurname(id: number, surname: string) {
     return await this.prisma.user.update({
-      where: { id: +id },data:{Surname: surname}
+      where: { id: +id }, data: { Surname: surname }
     });
   }
   async editBio(id: number, bio: string) {
     return await this.prisma.user.update({
-      where: { id: +id },data:{Bio: bio}
+      where: { id: +id }, data: { Bio: bio }
     });
   }
   async editVisibility(id: number, visibility: string) {
     let check = false;
-    if(visibility == "true")
-    {
+    if (visibility == "true") {
       check = true;
     }
     return await this.prisma.user.update({
-      where: { id: +id },data:{Visibility: check}
+      where: { id: +id }, data: { Visibility: check }
     });
   }
-  
+
   async deleteName(id: number) {
     return await this.prisma.user.update({
-      where: { id: +id },data:{Name: ''}
+      where: { id: +id }, data: { Name: '' }
     });
-  
+
   }
   async deleteSurname(id: number) {
     return await this.prisma.user.update({
-      where: { id: +id },data:{Surname: ''}
+      where: { id: +id }, data: { Surname: '' }
     });
   }
   async deleteBio(id: number) {
     return await this.prisma.user.update({
-      where: { id: +id },data:{Bio: ''}
+      where: { id: +id }, data: { Bio: '' }
     });
   }
   async deleteVisibility(id: number) {
     return await this.prisma.user.update({
-      where: { id: +id },data:{Visibility: false}
+      where: { id: +id }, data: { Visibility: false }
     });
   }
 }
