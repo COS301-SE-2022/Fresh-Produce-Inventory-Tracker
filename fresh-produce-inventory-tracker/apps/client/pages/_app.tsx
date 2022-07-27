d52c21a4d90200041c112a4a3e1b9deb3185eb2f
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-27 14:50:11 +0200

    Wrapped app with session provider

--- fresh-produce-inventory-tracker/apps/client/pages/_app.tsx
+++ fresh-produce-inventory-tracker/apps/client/pages/_app.tsx
@@ -1,18 +1,23 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../src/components/layout/layout';
-
+import { SessionProvider } from 'next-auth/react';
 import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+function CustomApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to the client!</title>
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
     </>
   );
 }
