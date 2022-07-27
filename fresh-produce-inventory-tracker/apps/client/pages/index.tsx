d41f8c501b04104b64e5c147507118377a7f3b9a
Awelani Murovhi <u18335412@tuks.co.za>
2022-07-27 14:51:25 +0200

    check if user is loggedin

--- fresh-produce-inventory-tracker/apps/client/pages/index.tsx
+++ fresh-produce-inventory-tracker/apps/client/pages/index.tsx
@@ -1,6 +1,31 @@
 import { MdDashboard } from 'react-icons/md';
 import Trends from '../src/components/trends';
+import { signIn, useSession } from 'next-auth/react';
+import { options } from './api/auth/[...nextauth]';
+import { unstable_getServerSession } from 'next-auth/next'
 
+export async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    options
+  );
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
 
 export function Index() {
   return (
