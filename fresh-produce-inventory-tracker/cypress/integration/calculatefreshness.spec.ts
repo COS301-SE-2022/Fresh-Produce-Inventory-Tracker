d5449c5bdb4d18508dd7784b75ac1dba0724e6d6
PapHein69 <u20436077@tuks.co.za>
2022-07-29 08:27:43 +0200

    Freshness integration
    integration testing for freshness

--- fresh-produce-inventory-tracker/cypress/integration/calculatefreshness.spec.ts
+++ fresh-produce-inventory-tracker/cypress/integration/calculatefreshness.spec.ts
@@ -4,3 +4,9 @@ const calculatefreshnessdata = {
     file : "files\\bread-7295.jpg"
   }
 
+  describe('Freshness tests', () => {
+    it('get freshnes prediction ',()=>{
+        cy.request('POST','http://localhost:3333/api/calcfreshness/predict',calculatefreshnessdata).as('getfresh');
+        
+    });
+})
