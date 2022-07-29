4df81b52bb961a33f3c8fe06cbf4cea6d92e9111
PapHein69 <u20436077@tuks.co.za>
2022-07-29 09:01:40 +0200

    Update integration test
    Update integration test for notification

--- fresh-produce-inventory-tracker/cypress/integration/notificationinttest.spec.ts
+++ fresh-produce-inventory-tracker/cypress/integration/notificationinttest.spec.ts
@@ -2,4 +2,9 @@ const notificationdata = {
     receiver : "heinrichvt06@gmail.com"
   }
 
-  
+  describe('Notofication tests', () => {
+    it('get Notification ',()=>{
+        cy.request('POST','http://localhost:3333/api/notification/send',notificationdata).as('getNotify');
+        
+    });
+})
