46f78d69c7c9b44ba76c31785c598c8b1ead95aa
Awelani Murovhi <u18335412@tuks.co.za>
2022-08-22 19:29:45 +0200

    Refactor inventory ui

--- fresh-produce-inventory-tracker/apps/client/pages/inventory/index.tsx
+++ fresh-produce-inventory-tracker/apps/client/pages/inventory/index.tsx
@@ -6,12 +6,16 @@ import { useState } from 'react';
 /* eslint-disable-next-line */
 export interface InventoryProps {}
 
-const SHOW_ITEMS: string[] = ['10 Items', '15 Items', '20 Items'];
+enum SHOW_ITEMS {
+  '10 Items' = '10 Items',
+  '15 Items' = '15 Items',
+  '20 Items' = '20 Items',
+}
 
 enum SELECT_STATUS {
-  Expired,
-  'About to Expire',
-  'NEW',
+  Expired = 'Expired',
+  'About to Expire' = 'About to Expire',
+  'New/Fresh' = 'New/Fresh',
 }
 
 export function Inventory(props: InventoryProps) {
@@ -19,14 +23,25 @@ export function Inventory(props: InventoryProps) {
 
   return (
     <div className="rounded-xl flex flex-wrap justify-between lg:max-w-[98%] px-4 py-10 bg-slate-50 mt-4 shadow-md">
-      <div className="flex flex-col-reverse gap-y-4 md:items-center md:flex-row gap-x-4">
-        <div className="flex items-center gap-x-5">
-          <span className="text-sm md:text-base">Show</span>
-          <Select SHOW_ITEMS={SHOW_ITEMS} />
+      <div className="flex flex-col-reverse gap-y-4 md:items-center md:flex-row gap-x-4 ">
+        <div className="flex items-center gap-x-5 w-fit">
+          <div className="dropdown">
+            <label tabIndex={0} className="m-1 text-white btn btn-primary">
+              Show
+            </label>
+            <ul
+              tabIndex={0}
+              className="p-4 space-y-4 text-white shadow dropdown-content bg-neutral menu rounded-box w-52"
+            >
+              {Object.keys(SHOW_ITEMS).map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </div>
         </div>
         <button
           onClick={() => setShowImageUpload(true)}
-          className="flex items-center justify-center w-full px-4 py-2 text-xs text-white rounded md:py-3 md:text-base gap-x-2 bg-primary"
+          className="flex items-center justify-center w-full px-4 py-2 text-xs text-white btn btn-primary md:py-3 md:text-base gap-x-2 bg-primary"
         >
           <IoAdd className="w-4 h-4 text-white md:flex" />
           <span>Add Item</span>
@@ -40,8 +55,19 @@ export function Inventory(props: InventoryProps) {
         />
       </div>
       <div className="flex items-center mt-2 w-fit gap-x-4 md:mt-0">
-        <p className="text-sm md:text-base">Status</p>
-        <Select SHOW_ITEMS={['Expired', 'About to expire', 'New/Fresh']} />
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="m-1 text-white btn btn-primary">
+            Status
+          </label>
+          <ul
+            tabIndex={0}
+            className="p-4 space-y-4 text-white shadow dropdown-content menu bg-neutral rounded-box w-52"
+          >
+            {Object.keys(SELECT_STATUS).map((status) => (
+              <li key={status}>{status}</li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <div className="w-full mt-10">
