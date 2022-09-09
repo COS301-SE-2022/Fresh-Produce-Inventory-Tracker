c5976216dcd545ca6bd809a06cade32b306153a8
Raymond Boonzaier <u19014725@tuks.co.za>
2022-09-09 07:11:34 +0200

    (service) delete image service function

--- fresh-produce-inventory-tracker/libs/api/image/service/src/lib/images.service.ts
+++ fresh-produce-inventory-tracker/libs/api/image/service/src/lib/images.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import {ImagesRepository} from '../../../repository/src/lib/images.repository';
-
+import * as fs from 'fs';
 @Injectable()
 export class ImagesService {
     constructor(private repo:ImagesRepository){}
     async uploadfile(user,filename:string){
         return await this.repo.createImage(user.id,filename);
+    }
+    async deletePicture(path: string) {
+     await fs.unlink(path, (err) => {
+      if (err) {
+       console.error(err);
+       return err;
+      }
+     });
     }
 }
