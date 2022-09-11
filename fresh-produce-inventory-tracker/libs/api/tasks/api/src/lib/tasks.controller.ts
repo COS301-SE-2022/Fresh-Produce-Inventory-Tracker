93776ace445f694fe6b40ca37b2c4bb5cade177a
Raymond Boonzaier <u19014725@tuks.co.za>
2022-09-11 19:22:16 +0200

    (tasks) updated for chages to notifications

--- fresh-produce-inventory-tracker/libs/api/tasks/api/src/lib/tasks.controller.ts
+++ fresh-produce-inventory-tracker/libs/api/tasks/api/src/lib/tasks.controller.ts
@@ -1,24 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, NotFoundException, Post } from '@nestjs/common';
 import { taskService } from '../../../service/src/lib/taskService.service';
 @Controller('tasks')
 export class tasksController {
   constructor(private taskService: taskService) {}
 
   @Post('gettasks')
-  async getTasks(
-    @Body('id') id: number,
-    
-  ) {
-    return await this.taskService.getTasks(+id);
+  async getTasks(@Body('id') id: number) {
+    try {
+      return await this.taskService.getTasks(+id);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
   @Post('createtask')
-  createTasks(@Body('id') id: number, @Body('message') message: string) {
-    return this.taskService.createTask(id,message);
+  createTasks(@Body('id') id: number, @Body('message') message: string,@Body('taskType') taskType:string,@Body('produceType') produceType:string) {
+    try {
+      return this.taskService.createTask(id, message,taskType,produceType);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
   @Post('deletetask')
   deleteTasks(@Body('id') id: number, @Body('message') message: string) {
-    return this.taskService.deleteTasks(id,message);
+    try {
+      return this.taskService.deleteTasks(id, message);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
- 
-
 }
