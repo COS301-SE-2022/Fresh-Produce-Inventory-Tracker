fec9086d2c3c2160d43a3356fb1aad13b8d15c3d
Raymond Boonzaier <u19014725@tuks.co.za>
2022-09-11 18:08:25 +0200

    (controller) added exception handling

--- fresh-produce-inventory-tracker/libs/api/profile/api/src/lib/profile.controller.ts
+++ fresh-produce-inventory-tracker/libs/api/profile/api/src/lib/profile.controller.ts
@@ -1,61 +1,100 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, NotFoundException, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { profileService } from '../../../service/src/lib/profile.service';
 @Controller('profile')
 export class profileController {
-  constructor(private service: profileService) {
-
-  }
+  constructor(private service: profileService) {}
 
   @Post('getprofile')
-  async getTasks(
-    @Body('token') token: string,
-  ) {
-    return this.service.getProfile(token);
+  async getTasks(@Body('token') token: string) {
+    try {
+      return this.service.getProfile(token);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Post('editprofile')
-  async editProfile(
-    @Body('token') token: string,
-    @Body('data') data: string,
-
-  ) {
-    return this.service.editProfile(token, data);
+  async editProfile(@Body('token') token: string, @Body('data') data: string) {
+    try {
+      return this.service.editProfile(token, data);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Post('editname')
   editName(@Body('id') id: number, @Body('name') name: string) {
-    return this.service.editName(id, name);
+    try {
+      return this.service.editName(id, name);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('editsurname')
   editSurname(@Body('id') id: number, @Body('surname') surname: string) {
-    return this.service.editSurname(id, surname);
+    try {
+      return this.service.editSurname(id, surname);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('editbio')
   editBio(@Body('id') id: number, @Body('bio') bio: string) {
-    return this.service.editBio(id, bio);
+    try {
+      return this.service.editBio(id, bio);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('editvisibility')
-  editVisibility(@Body('id') id: number, @Body('visibility') visibility: string) {
-    return this.service.editVisibility(id, visibility);
+  editVisibility(
+    @Body('id') id: number,
+    @Body('visibility') visibility: string
+  ) {
+    try {
+      return this.service.editVisibility(id, visibility);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('deletename')
   deleteName(@Body('id') id: number) {
-    return this.service.deleteName(id);
+    try {
+      return this.service.deleteName(id);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('deletesurname')
   deleteSurname(@Body('id') id: number) {
-    return this.service.deleteSurname(id);
+    try {
+      return this.service.deleteSurname(id);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('deletebio')
   deleteBio(@Body('id') id: number) {
-    return this.service.deleteBio(id);
+    try {
+      return this.service.deleteBio(id);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('deletevisibility')
   deleteVisibility(@Body('id') id: number) {
-    return this.service.deleteVisibility(id);
+    try {
+      return this.service.deleteVisibility(id);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
-
-
 }
-
-
