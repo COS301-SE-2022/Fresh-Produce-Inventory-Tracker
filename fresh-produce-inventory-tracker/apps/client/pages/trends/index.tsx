853e5939053fb5b2f014d229357ed46ea145c607
Durandt Uys <u18286250@tuks.co.za>
2022-09-27 14:12:32 +0200

    Remove redundent data

--- fresh-produce-inventory-tracker/apps/client/pages/trends/index.tsx
+++ fresh-produce-inventory-tracker/apps/client/pages/trends/index.tsx
@@ -11,7 +11,7 @@ import { useSession } from 'next-auth/react';
 
 const FreshProduce = [0,0,0,0,0,0,0,0,0,0,0,0];
 const PoultryMeat = [0,0,0,0,0,0,0,0,0,0,0,0];
-const Pastries = [0,0,0,0,0,0,0,0,0,0,0,0];
+const Pastries = [0,0,0,0,0,0,0,0,0,0,0];
 let FreshProduceLine = [];
 let PoultryMeatLine = [];
 let PastriesLine = [];
