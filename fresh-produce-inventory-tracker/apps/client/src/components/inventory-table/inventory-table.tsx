e77d5b612d5f39b12aa01d78a3ea3e90853f3882
Durandt Uys <u18286250@tuks.co.za>
2022-09-27 16:47:25 +0200

    Live Inventory Data

--- fresh-produce-inventory-tracker/apps/client/src/components/inventory-table/inventory-table.tsx
+++ fresh-produce-inventory-tracker/apps/client/src/components/inventory-table/inventory-table.tsx
@@ -1,107 +1,115 @@
 /* eslint-disable-next-line */
-export interface InventoryTableProps {}
+export interface InventoryTableProps {
+  data:any,
+  page:string
+}
 
 interface InventoryItem {
   id: number;
+  ProduceType:string;
   name: string;
-  description: string;
-  importDay: string;
-  condition: string;
-  expiresIn: string;
+  lastRestock: string;
+  produceStatus: string;
+  expireDate: string;
 }
 
-const INVENTORY_TABLE_DATA_MOCK: InventoryItem[] = [
-  {
-    id: 10,
-    name: 'Bell paper',
-    description: '10 Kgs of Bell papers',
-    importDay: 'Mar 13, 2022 at 10:01',
-    condition: 'good',
-    expiresIn: 'Apr 13, 2022',
-  },
-  {
-    id: 13,
-    name: 'Blue Berries',
-    description: '8 Kgs of Blue berries',
-    importDay: 'Mar 10, 2022 at 17:21',
-    condition: 'about to expire',
-    expiresIn: 'Mar 18, 2022',
-  },
-  {
-    id: 22,
-    name: 'Bread',
-    description: '10 loafs of bread',
-    importDay: 'Mar 01, 2022 at 09:11',
-    condition: 'good',
-    expiresIn: 'Mar 10, 2022',
-  },
-  {
-    id: 12,
-    name: 'Mangoes',
-    description: '32 Kgs of Mangoes',
-    importDay: 'Feb 23, 2022 at 12:32',
-    condition: 'expired',
-    expiresIn: 'Apr 22, 2022',
-  },
-  {
-    id: 52,
-    name: 'Pork',
-    description: '12 Kgs of pork',
-    importDay: 'Mar 14, 2022 at 10:01',
-    condition: 'good',
-    expiresIn: 'Apr 01, 2022',
-  },
-];
-
 export function InventoryTable(props: InventoryTableProps) {
-  return (
-    <div className="w-full ">
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          <thead>
-            <tr className="text-center text-primary/50">
-              <th>#ID</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Imported Day</th>
-              <th>Expiry Date Day</th>
-              <th>Condition</th>
-            </tr>
-          </thead>
-          <tbody>
-            {INVENTORY_TABLE_DATA_MOCK.map(
-              ({ id, name, description, expiresIn, condition, importDay }) => {
-                return (
-                  <tr key={id} className="text-sm text-center">
-                    <th>{id}</th>
-                    <td>{name}</td>
-                    <td>{description}</td>
-                    <td>{importDay}</td>
-                    <td>{expiresIn}</td>
-                    <td className="flex justify-center">
-                      <span
-                        className={`py-2 px-4 rounded-full ${
-                          condition === 'good'
-                            ? 'bg-success/20 text-green-800'
-                            : condition === 'about to expire'
-                            ? 'bg-warning/20 text-orange-700'
-                            : condition === 'expired'
-                            ? 'bg-error/20 text-rose-700'
-                            : ''
-                        }`}
-                      >
-                        {condition}
-                      </span>
-                    </td>
-                  </tr>
-                );
-              }
-            )}
-          </tbody>
-        </table>
+  if(props.page != "home")
+  {
+    return (
+      <div className="w-full ">
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr className="text-center text-primary/50">
+                <th>#ID</th>
+                <th>ProduceType</th>
+                <th>Name</th>
+                <th>Imported Day</th>
+                <th>Expiry Date Day</th>
+                <th>produceStatus</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.data.map(
+                ({ id,ProduceType, name, expireDate, produceStatus, lastRestock }) => {
+                  return (
+                    <tr key={id} className="text-sm text-center">
+                      <th>{id}</th>
+                      <td>{ProduceType}</td>
+                      <td>{name}</td>
+                      <td>{lastRestock}</td>
+                      <td>{expireDate}</td>
+                      <td className="flex justify-center">
+                        <span
+                          className={`py-2 px-4 rounded-full ${
+                            produceStatus === 'good'
+                              ? 'bg-success/20 text-green-800'
+                              : produceStatus === 'about to expire'
+                              ? 'bg-warning/20 text-orange-700'
+                              : produceStatus === 'expired'
+                              ? 'bg-error/20 text-rose-700'
+                              : ''
+                          }`}
+                        >
+                          {produceStatus}
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else
+  {
+    return (
+      <div className="w-full">
+        <div className="overflow-x-auto">
+          <table className="table w-half">
+            <thead>
+              <tr className="text-center text-primary/50">
+                <th>#ID</th>
+                <th>Name</th>
+                <th>State</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.data.map(
+                ({ id,name,produceStatus}) => {
+                  return (
+                    <tr key={id} className="text-sm text-center">
+                      <th>{id}</th>
+                      <td>{name}</td>
+                      <td className="flex justify-center">
+                        <span
+                          className={`py-2 px-4 rounded-full ${
+                            produceStatus === 'good'
+                              ? 'bg-success/20 text-green-800'
+                              : produceStatus === 'about to expire'
+                              ? 'bg-warning/20 text-orange-700'
+                              : produceStatus === 'expired'
+                              ? 'bg-error/20 text-rose-700'
+                              : ''
+                          }`}
+                        >
+                          {produceStatus}
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default InventoryTable;
