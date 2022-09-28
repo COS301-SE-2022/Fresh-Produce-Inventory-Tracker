f78c861fd5e43192de3b3708ea1627ca4fa81c3f
Durandt Uys <u18286250@tuks.co.za>
2022-09-28 09:48:00 +0200

    test

--- fresh-produce-inventory-tracker/apps/client/pages/about/index.tsx
+++ fresh-produce-inventory-tracker/apps/client/pages/about/index.tsx
@@ -6,7 +6,7 @@ export interface AboutProps {}
 export function About(props: AboutProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to About!</h1>
+      <h1>Welcome to About</h1>
     </div>
   );
 }
