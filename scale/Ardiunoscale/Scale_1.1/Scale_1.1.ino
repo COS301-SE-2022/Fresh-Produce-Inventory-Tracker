bf5759475eebdc41186fb36fd7842d8d7179dd0a
PapHein69 <u20436077@tuks.co.za>
2022-09-28 23:25:46 +0200

    Scale Updates
    Adjusted library and updated scales

---
+++ scale/Ardiunoscale/Scale_1.1/Scale_1.1.ino
@@ -0,0 +1,107 @@
+//Calibration = 22.50 BoardName = ACM1 Scale 2 Baud = 19200
+
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
+
+//pins:
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+const int tareOffsetVal_eepromAdress = 4;
+unsigned long t = 0;
+float oldvalue = 0 ;
+
+void setup() {
+  Serial.begin(19200); delay(10);
+  //Serial.println();
+  //Serial.println("Starting...");
+
+  LoadCell.begin();
+  //LoadCell.setReverseOutput();
+  float calibrationValue; // calibration value (see example file "Calibration.ino")
+  calibrationValue = 22.50; // uncomment this if you want to set the calibration value in the sketch
+
+#if defined(ESP8266)|| defined(ESP32)
+  EEPROM.begin(512);
+#endif
+
+  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+
+  //restore the zero offset value from eeprom:
+  long tare_offset = 0;
+  EEPROM.get(tareOffsetVal_eepromAdress, tare_offset);
+  LoadCell.setTareOffset(tare_offset);
+  boolean _tare = false; //set this to false as the value has been resored from eeprom
+
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    //Serial.println("Startup is complete");
+  }
+}
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 10000; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    float i = LoadCell.getData();
+    if (millis() > t + serialPrintInterval ) { //||i<oldvalue-100 || i>oldvalue+100
+      
+
+      
+        unsigned int k = i;
+ 
+        String strClose = String(")");
+        String strOpen = String("(");
+        String test = String() ;
+      //Serial.println('('+k+')'); //gives output
+      test = strOpen + k + strClose ;
+      Serial.print(test); //gives output
+      oldvalue = i;
+        
+      
+      
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  // receive command from serial terminal, send 't' to initiate tare operation:
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') refreshOffsetValueAndSaveToEEprom();
+  }
+}
+
+// zero offset value (tare), calculate and save to EEprom:
+void refreshOffsetValueAndSaveToEEprom() {
+  long _offset = 0;
+  //Serial.println("Calculating tare offset value...");
+  LoadCell.tare(); // calculate the new tare / zero offset value (blocking)
+  _offset = LoadCell.getTareOffset(); // get the new tare / zero offset value
+  EEPROM.put(tareOffsetVal_eepromAdress, _offset); // save the new tare / zero offset value to EEprom
+#if defined(ESP8266) || defined(ESP32)
+  EEPROM.commit();
+#endif
+  LoadCell.setTareOffset(_offset); // set value as library parameter (next restart it will be read from EEprom)
+  //Serial.print("New tare offset value:");
+  //Serial.print(_offset);
+  //Serial.print(", saved to EEprom adr:");
+  //Serial.println(tareOffsetVal_eepromAdress);
+}
