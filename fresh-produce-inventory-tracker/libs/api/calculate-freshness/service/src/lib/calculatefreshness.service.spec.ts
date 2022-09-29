c05937cb378d75ec93759dfe839c740ab1c10edb
Michael Geere <mgeere10@gmail.com>
2022-09-29 08:41:11 +0200

    (unit tests) Added getAllModels test in CF service

--- fresh-produce-inventory-tracker/libs/api/calculate-freshness/service/src/lib/calculatefreshness.service.spec.ts
+++ fresh-produce-inventory-tracker/libs/api/calculate-freshness/service/src/lib/calculatefreshness.service.spec.ts
@@ -2,6 +2,11 @@ import { Test } from '@nestjs/testing';
 import { PrismaService } from '../../../../prisma/shared/src/lib/prismaService.service';
 import { calculatefreshnessService } from './calculatefreshness.service';
 
+class model {
+    model: string[];
+}
+
+const MockModelArr: jest.Mocked<model> = new model() as model;
 describe('ApiAuthorizationRepositoryTest', () => {
     let data: calculatefreshnessService;
 
@@ -12,6 +17,16 @@ describe('ApiAuthorizationRepositoryTest', () => {
         }).compile();
 
         data = moduleRef.get<calculatefreshnessService>(calculatefreshnessService);
+    });
 
+    it('should get all models', async () => {
+        jest
+            .spyOn(data, 'getAllModels')
+            .mockImplementation(
+                (): Promise<null> => Promise.resolve(null)
+            );
+        expect(
+            await data.getAllModels()
+        ).toBe(MockModelArr);
     });
 });
