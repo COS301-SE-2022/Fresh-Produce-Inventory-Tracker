69f639b5db3ff32155df3e8229870af666c2e7c0
Michael Geere <mgeere10@gmail.com>
2022-09-29 08:46:08 +0200

    (unit tests) Finished CF service
    Added predict test.

--- fresh-produce-inventory-tracker/libs/api/calculate-freshness/service/src/lib/calculatefreshness.service.spec.ts
+++ fresh-produce-inventory-tracker/libs/api/calculate-freshness/service/src/lib/calculatefreshness.service.spec.ts
@@ -6,7 +6,14 @@ class model {
     model: string[];
 }
 
+class freshness {
+    id: number;
+    type: string;
+    file: string;
+}
+
 const MockModelArr: jest.Mocked<model> = new model() as model;
+const mockFreshness: jest.Mocked<freshness> = new freshness() as freshness;
 describe('ApiAuthorizationRepositoryTest', () => {
     let data: calculatefreshnessService;
 
@@ -28,5 +35,16 @@ describe('ApiAuthorizationRepositoryTest', () => {
         expect(
             await data.getAllModels()
         ).toBe(MockModelArr);
+    });
+
+    it('should get predict', async () => {
+        jest
+            .spyOn(data, 'predict')
+            .mockImplementation(
+                (): Promise<null> => Promise.resolve(null)
+            );
+        expect(
+            await data.predict(1, 'fruit', '//files/banana.png')
+        ).toBe(mockFreshness);
     });
 });
